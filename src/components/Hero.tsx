cefe081d9d654f577940d8220565a3473b0c6f09
suttirakcha <suttirak.char@gmail.com>
2026-08-24 07:35:07 +0700

    fix: profile image preview

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -1,6 +1,8 @@
-import profileImg from "../assets/suttirak-ch-profile-image.jpg";
-
 function Hero() {
+  const profileImg = new URL(
+    "../assets/suttirak-ch-profile-image.jpg",
+    import.meta.url
+  ).href;
   return (
     <section className="max-w-6xl mx-auto px-6 pt-32 pb-24 grid md:grid-cols-[1fr_auto] gap-16 items-start">
       <div className="flex flex-col gap-6">
